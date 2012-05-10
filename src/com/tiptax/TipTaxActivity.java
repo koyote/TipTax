ebd7423769a97d3e88a553ba6fe5a371228a99c4
Pit Apps <>
2012-05-09 21:53:39 -0700

    Initial Commit

---
+++ src/com/tiptax/TipTaxActivity.java
@@ -0,0 +1,13 @@
+package com.tiptax;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TipTaxActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
