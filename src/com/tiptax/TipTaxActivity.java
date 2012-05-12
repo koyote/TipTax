0c5efee379ede986d98b96f3b0e66bfceb08d67c
jonathanbibas <jonathanbibas@dhcp-248-191.cruznetsecure.ucsc.edu>
2012-05-11 23:46:54 -0700

    Improvements in readability, and modifiable rows.

--- src/com/tiptax/TipTaxActivity.java
+++ src/com/tiptax/TipTaxActivity.java
@@ -4,15 +4,11 @@ import java.text.DecimalFormat;
 
 import android.app.Activity;
 import android.app.Dialog;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
-import android.view.ViewGroup.LayoutParams;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.LinearLayout;
-import android.widget.ListView;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
@@ -21,8 +17,9 @@ public class TipTaxActivity extends Activity {
 
 	private TableLayout mainTable;
 	private Button addPerson;
-	private double totalSum, totalSumPeople;
-	private double tipPercentage, taxPercentage;
+	private TextView totalDue, tipDue, taxDue;
+	private double totalSumPeople;
+	private double tipPercentage;
 
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -30,88 +27,158 @@ public class TipTaxActivity extends Activity {
 
 		addPerson = (Button) findViewById(R.id.addPerson);
 		mainTable = (TableLayout) findViewById(R.id.mainTable);
+		totalDue = (TextView) findViewById(R.id.TotalDueText);
+		tipDue = (TextView) findViewById(R.id.tipInput);
+		taxDue = (TextView) findViewById(R.id.taxInput);
 		tipPercentage = 20;
-		taxPercentage = 15;
 
 		addPerson.setOnClickListener(new View.OnClickListener() {
 
 			public void onClick(View v) {
-				// TODO Auto-generated method stub
 				// create the dialog box
 				final Dialog dialog = new Dialog(TipTaxActivity.this);
-
-				totalSum=0;
 
 				dialog.setContentView(R.layout.add_person);
 				dialog.setTitle("Add you desired Item");
 
 				dialog.show();
 
-				final EditText name = (EditText) dialog
-						.findViewById(R.id.namePerson);
-				final EditText totalPerson = (EditText) dialog
-						.findViewById(R.id.totalSpent);
-				final TextView totalDue = (TextView) findViewById(R.id.TotalDueText);
-				final TextView tipDue = (TextView) findViewById(R.id.tipInput);
-				final TextView taxDue = (TextView) findViewById(R.id.taxInput);
-				Button okInput = (Button) dialog.findViewById(R.id.OKinput);
+				final EditText nameDialog = (EditText) dialog.findViewById(R.id.namePerson);
+				final EditText totalDialog = (EditText) dialog.findViewById(R.id.totalSpent);
+				final Button okDialog = (Button) dialog.findViewById(R.id.OKinput);
 
-				okInput.setOnClickListener(new View.OnClickListener() {
+				okDialog.setOnClickListener(new View.OnClickListener() {
 
 					public void onClick(View v) {
 						// TODO Auto-generated method stub
 						dialog.dismiss();
-						if(!totalPerson.getText().toString().equals("")){//safety of no input
-							
-							//add a new row to the table
-							TableRow tr1 = new TableRow(TipTaxActivity.this);
-							TextView textviewName = new TextView(
-									TipTaxActivity.this);
-							textviewName.setText(name.getText().toString());
-							textviewName.setTextSize(25);
-							textviewName.setGravity(Gravity.LEFT);
-							textviewName.setPadding(10, 10, 10, 10);
-							TextView textviewTotal = new TextView(
-									TipTaxActivity.this);
-							textviewTotal.setText(totalPerson.getText().toString());
-							textviewTotal.setTextSize(25);
-							textviewTotal.setGravity(Gravity.RIGHT);
-							textviewName.setPadding(10, 10, 10, 10);
-							tr1.addView(textviewName);
-							tr1.addView(textviewTotal);
-							mainTable.addView(tr1);
+						// safety if no input
+						if (!totalDialog.getText().toString().equals("")) {
+							final String nameDialogText = nameDialog.getText().toString();
+							final String sumDialogText = totalDialog.getText().toString();
 
+							final TextView nameOfRowEditText = new TextView(TipTaxActivity.this);
+							final TextView sumOfRowEditText = new TextView(TipTaxActivity.this);
 
+							nameOfRowEditText.setText(nameDialogText);
+							sumOfRowEditText.setText(sumDialogText);
+							TableRow newRow = createRow(nameOfRowEditText, sumOfRowEditText);
 
-							//calculations to get the total sum
-							totalSumPeople += Double.parseDouble(totalPerson.getText()
-									.toString());
+							mainTable.addView(newRow);
 
+							// Listener for every row added
+							newRow.setOnClickListener(new View.OnClickListener() {
 
-							double tipToPay = tipPercentage*totalSumPeople/100;
-							double taxToPay = taxPercentage*totalSumPeople/100;
+								public void onClick(View v) {
+									// TODO Auto-generated method stub
+									final Dialog dialog = new Dialog(TipTaxActivity.this);
 
-							totalSum = totalSumPeople + taxToPay;
-							totalSum += tipToPay;
-							
-							//Rounding two Decimal point
-							DecimalFormat twoDForm = new DecimalFormat("#.##");
-							totalSum = Double.valueOf(twoDForm.format(totalSum));
-							tipToPay = Double.valueOf(twoDForm.format(tipToPay));
-							taxToPay = Double.valueOf(twoDForm.format(taxToPay));
+									dialog.setContentView(R.layout.add_person);
+									dialog.setTitle("Change values");
 
-							//sets the values to the textEdits
-							totalDue.setText(Double.toString(totalSum));
-							tipDue.setText(Double.toString(tipToPay));
-							taxDue.setText(Double.toString(taxToPay));
+									final EditText nameDialog = (EditText) dialog.findViewById(R.id.namePerson);
+									final EditText totalDialog = (EditText) dialog.findViewById(R.id.totalSpent);
+									final Button okDialog = (Button) dialog.findViewById(R.id.OKinput);
+
+									nameDialog.setText(nameDialogText);
+									totalDialog.setText(sumDialogText);
+
+									dialog.show();
+
+									okDialog.setOnClickListener(new View.OnClickListener() {
+
+										public void onClick(View v) {
+											// TODO Auto-generated method stub
+
+											dialog.dismiss();
+
+											if (!nameDialog.getText().toString().equals("")) {
+												// substract ols value and adds
+												// the new one
+												totalSumPeople -= Double.parseDouble(sumDialogText);
+												totalSumPeople += Double.parseDouble(totalDialog.getText().toString());
+
+												nameOfRowEditText.setText(nameDialog.getText().toString());
+												sumOfRowEditText.setText(totalDialog.getText().toString());
+
+												updateTotalAndTipValues(totalSumPeople, tipPercentage);
+											}
+										}
+									});
+
+								}
+							});
+
+							// calculations to get the total sum
+							totalSumPeople += Double.parseDouble(sumDialogText);
+
+							// updates the total values
+							updateTotalAndTipValues(totalSumPeople, tipPercentage);
+
 						}
 					}
 				});
 
-
-
 			}
 		});
+
+		// listener to change the taxRow value
+		taxDue.setOnClickListener(new View.OnClickListener() {
+
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				final Dialog dialog = new Dialog(TipTaxActivity.this);
+				dialog.setContentView(R.layout.add_person);
+				dialog.setTitle("Change values");
+
+				final EditText name = (EditText) dialog.findViewById(R.id.namePerson);
+				name.setText("TAX");
+
+				final EditText taxTotal = (EditText) dialog.findViewById(R.id.totalSpent);
+				final Button okButton = (Button) dialog.findViewById(R.id.OKinput);
+
+				dialog.show();
+
+				okButton.setOnClickListener(new View.OnClickListener() {
+
+					public void onClick(View v) {
+						// TODO Auto-generated method stub
+						dialog.dismiss();
+						if (!taxTotal.getText().toString().equals("")) {
+							taxDue.setText(taxTotal.getText().toString());
+							updateTotalAndTipValues(totalSumPeople, tipPercentage);
+						}
+					}
+				});
+			}
+		});
+
+	}
+
+	private TableRow createRow(TextView name, TextView sum) {
+
+		// add a new row to the table
+		TableRow tr1 = new TableRow(TipTaxActivity.this);
+		name.setTextSize(25);
+		name.setGravity(Gravity.LEFT);
+		name.setPadding(10, 10, 10, 10);
+		sum.setTextSize(25);
+		sum.setGravity(Gravity.RIGHT);
+		sum.setPadding(10, 10, 10, 10);
+		tr1.addView(name);
+		tr1.addView(sum);
+		return tr1;
+
+	}
+
+	private void updateTotalAndTipValues(double totalSumPeople, double tipPercentage) {
+		// Rounding two Decimal point
+		DecimalFormat twoDForm = new DecimalFormat("#.##");
+		Double taxValue = Double.parseDouble(taxDue.getText().toString());
+		double result = totalSumPeople + taxValue + (tipPercentage * totalSumPeople / 100.0);
+		result = Double.valueOf(twoDForm.format(result));
+		tipDue.setText(Double.toString(Double.valueOf(twoDForm.format(tipPercentage * totalSumPeople / 100.0))));
+		totalDue.setText(Double.toString(result));
 
 	}
 
