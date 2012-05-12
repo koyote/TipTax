7011b241707d2407b319894db7a3f7b7012cc4fb
jonathanbibas <jonathanbibas@dhcp-248-191.cruznetsecure.ucsc.edu>
2012-05-11 17:59:52 -0700

    bothing

--- src/com/tiptax/TipTaxActivity.java
+++ src/com/tiptax/TipTaxActivity.java
@@ -8,10 +8,7 @@ public class TipTaxActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-        
-        System.out
-        
+        setContentView(R.layout.main);        
         
     }
 }
